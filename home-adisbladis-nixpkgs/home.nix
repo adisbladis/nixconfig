88542b8a7d8257793982f1b2e8d8e46e9f7f2552
adisbladis <adis@blad.is>
2017-06-30 23:55:29 +0800

    Add emacs packages

--- home-adisbladis-nixpkgs/home.nix
+++ home-adisbladis-nixpkgs/home.nix
@@ -35,9 +35,37 @@
     extraPackages = epkgs: [
       epkgs.nix-mode
       epkgs.magit
+      epkgs.zerodark-theme
       pkgs.python36
       pkgs.python36Packages.jedi
       pkgs.python36Packages.epc
+      epkgs.jedi
+      epkgs.fish-mode
+      epkgs.jinja2-mode
+      epkgs.lua-mode
+      epkgs.irony
+      epkgs.rust-mode
+      epkgs.android-mode
+      epkgs.markdown-mode
+      epkgs.go-mode
+      epkgs.yaml-mode
+      epkgs.web-mode
+      epkgs.company
+      epkgs.company-flx
+      epkgs.company-statistics
+      epkgs.company-go
+      epkgs.ag
+      epkgs.flx-ido
+      epkgs.smooth-scrolling
+      epkgs.swiper
+      epkgs.webpaste
+      epkgs.smart-mode-line
+      epkgs.smart-mode-line-powerline-theme
+      epkgs.flycheck
+      epkgs.flycheck-irony
+      epkgs.flycheck-mypy
+      epkgs.flycheck-rust
+      epkgs.smartparens
     ];
   };
 
