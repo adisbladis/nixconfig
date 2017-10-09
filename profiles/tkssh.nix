404d3ae48afdbd4e00dae6fcff69b806710187f1
adisbladis <adis@blad.is>
2017-10-10 03:36:53 +0800

    Tk overlay upstream changed layout

--- profiles/tkssh.nix
+++ profiles/tkssh.nix
@@ -1,12 +1,12 @@
 {
   # To install packages in systemPackages
   nixpkgs.overlays = [
-    (import ../overlays/nixpkgs-trustedkey/pkgs/default.nix)
+    (import ../overlays/nixpkgs-trustedkey/default.nix)
   ];
 
   # To use the provided services (tk-ssh-agent)
   imports = [
-    ../overlays/nixpkgs-trustedkey/default.nix
+    ../overlays/nixpkgs-trustedkey/modules/default.nix
   ];
 
   services.tk-ssh-agent.enable = true;
