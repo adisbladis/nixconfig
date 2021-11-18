a8cddf003d48bb2469c7e2e5f8098c0209cafa0d
adisbladis <adisbladis@gmail.com>
2021-11-17 18:47:13 -0800

    Run nixpkgs-fmt

--- modules/nix.nix
+++ modules/nix.nix
@@ -18,14 +18,16 @@
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
-    nixPath = let
-      overlays = pkgs.writeText "overlays.nix" ''
-        [ (import ${builtins.fetchGit ../third_party/emacs-overlay}) ]
-      '';
-    in [
-      "nixpkgs=${builtins.fetchGit pkgs.path}"
-      "nixpkgs-overlays=${overlays}"
-    ];
+    nixPath =
+      let
+        overlays = pkgs.writeText "overlays.nix" ''
+          [ (import ${builtins.fetchGit ../third_party/emacs-overlay}) ]
+        '';
+      in
+      [
+        "nixpkgs=${builtins.fetchGit pkgs.path}"
+        "nixpkgs-overlays=${overlays}"
+      ];
     autoOptimiseStore = true;
     useSandbox = true;
     trustedUsers = [ "@wheel" ];
