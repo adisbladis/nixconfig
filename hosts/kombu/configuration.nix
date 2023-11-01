1a68d03e7f55c8ec8a39ee9c26ded6a6788ce743
adisbladis <adisbladis@gmail.com>
2023-11-01 17:13:36 +1300

    kombu: Add EPYC overclocking

--- hosts/kombu/configuration.nix
+++ hosts/kombu/configuration.nix
@@ -17,6 +17,33 @@
 
   time.timeZone = "Pacific/Auckland";
 
+  # Overclock EPYC
+  boot.kernelModules = [ "msr" ];
+  systemd.services.zenstates = let
+    zenstates = let
+      src = pkgs.fetchFromGitHub {
+        owner = "r4m0n";
+        repo = "ZenStates-Linux";
+        rev = "0bc27f4740e382f2a2896dc1dabfec1d0ac96818";
+        sha256 = "1h1h2n50d2cwcyw3zp4lamfvrdjy1gjghffvl3qrp6arfsfa615y";
+      };
+    in pkgs.runCommand "zenstates" { buildInputs = [ pkgs.python3 ]; } ''
+      mkdir -p $out/bin
+      cp ${src}/zenstates.py $out/bin/zenstates
+      chmod +x $out/bin/zenstates
+      patchShebangs $out/bin/zenstates
+    '';
+  in {
+    description = "Dynamically edit AMD Ryzen/EPYC processor P-States";
+    serviceConfig = {
+      # Note: Overclocks P0 state to:
+      # P0 - Enabled - FID = 40 - DID = 4 - VID = 49 - Ratio = 32.00 - vCore = 1.09375
+      ExecStart = "${zenstates}/bin/zenstates -p 0 -f 40 -v 49 -d 4";
+      Type = "oneshot";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
   home-manager.users.adisbladis = { ... }: {
     home.stateVersion = "23.11";
     # Run xmodmap _after_ home-managers setxkbmap service, otherwise we'll get undone
